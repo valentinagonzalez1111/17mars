e66a949571d997a52c4dd18298e79e7aff9e99d9
Valentina <balendi.art@gmail.com>
2023-03-17 09:40:07 +0100

    Initial Commit

---
+++ 17mars/_7marsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _7marsApp.swift
+//  17mars
+//
+//  Created by Apprenant 18 on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct _7marsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
