61308065898546054aec98bacf8efbf526707401
Valentina <balendi.art@gmail.com>
2023-03-17 10:22:29 +0100

    weather

--- 17mars/ContentView.swift
+++ 17mars/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("SF Symbol weather list")
+                .font(.largeTitle)
+                .padding()
+            SymbolLine(symbolName: "sun.min.fill", color: .blue)
+            SymbolLine(symbolName: "sun.max.fill", color: .gray)
+            SymbolLine(symbolName: "sunrise.fill", color: .green)
+            SymbolLine(symbolName: "sunset.fill", color: .orange)
+            SymbolLine(symbolName: "sun.dust.fill", color: .pink)
+            SymbolLine(symbolName: "sun.haze.fill", color: .purple)
+            SymbolLine(symbolName: "cloud.fill", color: .red)
+            SymbolLine(symbolName: "cloud.drizzle.fill", color: .yellow)
         }
-        .padding()
+    }
+}
+
+
+struct SymbolLine: View {
+    let symbolName: String
+    let color: Color
+    
+    var body: some View {
+        HStack {
+            Image(systemName: symbolName)
+            Text(symbolName)
+        }
+        .font(.title)
+        .foregroundColor(color)
     }
 }
 
